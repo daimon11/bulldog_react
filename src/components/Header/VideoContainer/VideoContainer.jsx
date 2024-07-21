9fe900a91df9c4ae7f03253df139b9602bd438a5
Dmitriy Bezgodko <daimon.94@mail.ru>
2024-07-21 22:22:57 +0300

    change video

--- src/components/Header/VideoContainer/VideoContainer.jsx
+++ src/components/Header/VideoContainer/VideoContainer.jsx
@@ -1,18 +1,41 @@
+import {useState, useLayoutEffect} from 'react';
 import style from '../Header.module.scss';
 
-import video from '../../../video/bulldogpub_murmansk.mp4';
+// import video from '../../../video/bulldogpub_murmansk.mp4';
+import video1 from '../../../video/bulldogpub_murmansk.mp4';
+import video2 from '../../../video/bulldogpub_murmansk_1000.webm';
 
-export const VideoContainer = () => <div
-  className={style.header__video_container}>
-  <video
-    className={style.header__video}
-    autoPlay
-    loop
-    muted
-    playsInline
-    poster
-    preload='none'
-    src={video}>
-  </video>
-</div>;
+export const VideoContainer = () => {
+  const [videoSrc, setVideoSrc] = useState(video1);
+
+  useLayoutEffect(() => {
+    const updateVideoSrc = () => {
+      if (window.innerWidth <= 600) {
+        setVideoSrc(video2);
+      } else {
+        setVideoSrc(video1);
+      }
+    };
+    updateVideoSrc();
+    window.addEventListener('resize', updateVideoSrc);
+
+    return () => {
+      window.removeEventListener('resize', updateVideoSrc);
+    };
+  }, []);
+
+  return <div
+    className={style.header__video_container}>
+    <video
+      className={style.header__video}
+      autoPlay
+      loop
+      muted
+      playsInline
+      poster
+      preload='none'
+      src={videoSrc}>
+    </video>
+  </div>;
+};
 
