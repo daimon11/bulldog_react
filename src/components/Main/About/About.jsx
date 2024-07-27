4c131f704b82351fbd81807eb27fa4b74da5246b
Dmitriy Bezgodko <daimon.94@mail.ru>
2024-07-27 22:56:15 +0300

    add slider

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -1,4 +1,7 @@
+import React, {useState, useEffect} from 'react';
+
 import classNames from 'classnames';
+import {Splide, SplideSlide, SplideTrack} from '@splidejs/react-splide';
 
 import style from './About.module.scss';
 
@@ -7,7 +10,19 @@ import {photos} from './PhotoItem/photoList';
 import {PhotoItem} from './PhotoItem/PhotoItem';
 
 export const About = () => {
-  console.log(style);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 600);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return <article
     className={style.about}>
@@ -20,31 +35,52 @@ export const About = () => {
           className={style.about__description}>
           <div className={style.about__paragraf_wrapper}>
             <p className={style.about__text}>
-            Добро пожаловать в паб &quot;Бульдог&quot; – ваше место для наслаждения спортивными трансляциями и изысканной кухней в Мурманске. Современные технологии и классический английский стиль создают неповторимую атмосферу для дружеских встреч и романтических свиданий.
+              Добро пожаловать в паб &quot;Бульдог&quot; – ваше место для наслаждения спортивными трансляциями и изысканной кухней в Мурманске. Современные технологии и классический английский стиль создают неповторимую атмосферу для дружеских встреч и романтических свиданий.
             </p>
             <p className={style.about__text}>
-            Спортивные трансляции на новом уровне. Наслаждайтесь каждым моментом игры на 8 больших экранах FullHD с современной стерео-системой в компании друзей.
+              Спортивные трансляции на новом уровне. Наслаждайтесь каждым моментом игры на 8 больших экранах FullHD с современной стерео-системой в компании друзей.
             </p>
             <p className={style.about__text}>
-            Изысканная кухня и широкий ассортимент виски. Отведайте блюда, которые порадуют гурманов, и исследуйте самый большой выбор виски в городе. А специальное хранилище для пива гарантирует идеальный вкус и температуру напитков.
+              Изысканная кухня и широкий ассортимент виски. Отведайте блюда, которые порадуют гурманов, и исследуйте самый большой выбор виски в городе. А специальное хранилище для пива гарантирует идеальный вкус и температуру напитков.
             </p>
             <p className={style.about__text}>
-            Летняя терраса с панорамным видом. Расслабьтесь на нашей террасе с видом на Мурманск и Кольский залив в теплое время года.
+              Летняя терраса с панорамным видом. Расслабьтесь на нашей террасе с видом на Мурманск и Кольский залив в теплое время года.
             </p>
             <p className={style.about__text}>
-            Паб &quot;Бульдог&quot; ждет вас, чтобы предложить лучшее из мира английских пабов в Мурманске.
+              Паб &quot;Бульдог&quot; ждет вас, чтобы предложить лучшее из мира английских пабов в Мурманске.
             </p>
           </div>
           <button className={style.about__btn_detail}>Подробнее</button>
         </div>
 
-        <div className={classNames(style.about__photos, style.about__columns)}>
-          {photos.map((item, index) => <PhotoItem
-            data={item}
-            key={index}
-            pos={index} />)}
-        </div>
-
+        {isMobile ? (<Splide
+          hasTrack={false}
+          options={{
+            type: 'loop',
+            role: 'tab',
+            height: 350,
+            arrows: true,
+          }}>
+          <SplideTrack>
+            {photos.map((item, index) =>
+              <SplideSlide
+                key={index}
+                pos={index}
+              >
+                <PhotoItem
+                  data={item} />
+              </SplideSlide>)}
+          </SplideTrack>
+        </Splide>) :
+          (<div className={classNames(style.about__photos, style.about__columns)}>
+            {photos.map((item, index) =>
+              <PhotoItem
+                key={index}
+                pos={index}
+                data={item} />
+            )}
+          </div>)
+        }
       </div>
 
     </div>
--- src/components/Main/About/PhotoItem/PhotoItem.jsx
+++ src/components/Main/About/PhotoItem/PhotoItem.jsx
@@ -1,18 +1,21 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+
 import style from '../About.module.scss';
 
 export const PhotoItem = ({data}) => {
   console.log('props.index', data.img);
 
-  return <picture>
-    <source srcSet={data.imgAvif} type='image/avif' />
-    <source srcSet={data.imgWebp} type='image/webp' />
-    <img
-      src={data.img}
-      className={classNames(style.about__pic)} />
-  </picture>;
+  return (
+    <picture>
+      <source srcSet={data.imgAvif} type='image/avif' />
+      <source srcSet={data.imgWebp} type='image/webp' />
+      <img
+        src={data.img}
+        className={classNames(style.about__pic)} />
+    </picture>
+  );
 };
 
 
